45090f2f85724cb65911534d3cbd55dd451d64c7
Jurien Hamaker <whoami@jurien.dev>
2024-02-11 22:26:40 +0100

    wezterm

---
+++ config.lua
@@ -0,0 +1,17 @@
+local wezterm = require('wezterm')
+local colors = require('lua/rose-pine').colors()
+local window_frame = require('lua/rose-pine').window_frame()
+local config = wezterm.config_builder()
+
+config.default_prog = { 'C:\\Windows\\system32\\wsl.exe', '-d', 'Arch', '--cd', '~' }
+
+config.colors = colors
+config.window_frame =  window_frame
+
+config.window_decorations = "RESIZE" 
+config.enable_tab_bar = false
+
+config.window_close_confirmation = "NeverPrompt"
+
+config.font = wezterm.font('JetBrainsMono NF')
+return config
